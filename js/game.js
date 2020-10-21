cf589deed6e71470b25b20d196f5c36d7646d306
enochcho <enochcho97@hotmail.com>
2020-10-21 17:43:12 -0400

    something

--- js/game.js
+++ js/game.js
@@ -55,56 +55,6 @@ function Weapon(name) {
     this.damage = 1;
   }
 }
-<<<<<<< HEAD
-
-function Map(player){
-
-    this.spaces=[[]];
-    this.player = player;
-    this.currentX = 5;
-    this.currentY = 5;
-    this.rooms = 0;
-    this.complete = 0;
-    this.score = 30;
-
-    //fillSpaces method
-    const x = Math.floor(Math.random()*5);
-    const y = Math.floor(Math.random()*5);
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length; j++) {
-            if(((i===0 || i===4) && j===2)|| (i===2 && (j===0||j===4))){
-                spaces[i][j] = new Room();
-                this.rooms++;
-            } else{
-                spaces[i][j] = new Space(); 
-            }
-            
-        }//end of inner for loop
-    }//end of outer for loop
-    spaces[2][2].setCurrent = true;
-    this.currentX = 2;
-    this.currentY =2;
-    spaces[y][x].weapon=Weapon.setWeapon('sword');
-    //implement checkWeapon here since it can't be called.
-    if(x === 2 && y ===2){
-        player.weapon = spaces[x][y].weapon;
-        spaces[y][x].weapon = null;
-        //console.log("you got the sword!!!");
-        document.getElementById('Message').innerText = "You got the sword!!!";
-    }
-    //don't need getters and setters
-
-    this.toString= function(){
-        let str = "";
-        for(const space of spaces){
-            str += Arrays.toString(space) + "\n";
-        }
-        return str;
-    }
-
-
-    this.reduceScore= function(){
-=======
 class Map{
     constructor(player){
         this.spaces=[[]];
@@ -144,7 +94,6 @@ class Map{
     //don't need getters and setters
 
     reduceScore(){
->>>>>>> 8388eb2923a03375ee3f35e2d4f4184b0148b572
         this.score--;
     }
 
