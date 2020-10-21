46471eb811d0f4ab868b820558d642bf4744f787
enochcho <enochcho97@hotmail.com>
2020-10-21 17:59:26 -0400

    clock out

--- js/game.js
+++ js/game.js
@@ -55,47 +55,41 @@ function Weapon(name) {
     this.damage = 1;
   }
 }
-class Map{
-    constructor(player){
-        this.spaces=[[]];
-        this.player = player;
-        this.currentX = 5;
-        this.currentY = 5;
-        this.rooms = 0;
-        this.complete = 0;
-        this.score = 30;
 
-        //fillSpaces method
-        const x = Math.floor(Math.random()*5);
-        const y = Math.floor(Math.random()*5);
-        for (let i = 0; i < array.length; i++) {
-            for (let j = 0; j < array.length; j++) {
-                if(((i===0 || i===4) && j===2)|| (i===2 && (j===0||j===4))){
-                    spaces[i][j] = new Room();
-                    this.rooms++;
-                } else{
-                    spaces[i][j] = new Space(); 
-                }
-                
-            }//end of inner for loop
-        }//end of outer for loop
-        spaces[2][2].setCurrent = true;
-        this.currentX = 2;
-        this.currentY =2;
-        spaces[y][x].weapon= new Weapon('sword');
-        //implement checkWeapon here since it can't be called.
-        if(x === 2 && y ===2){
-            player.weapon = spaces[x][y].weapon;
-            spaces[y][x].weapon = null;
-            //console.log("you got the sword!!!");
-            document.getElementById('Message').innerText = "You got the sword!!!";
-        }
-    } 
+let rooms = 0;
+let complete = 0;
+let score = 30;
+let currentX = 5;
+let currentY = 5;
+let spaces = [[]];
+
+function fillSpaces(){
+    const x = Math.floor(Math.random()*5);
+    const y = Math.floor(Math.random()*5);
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if(((i===0 || i===4) && j===2)|| (i===2 && (j===0||j===4))){
+                spaces[i][j] = new Room();
+                this.rooms++;
+            } else{
+                spaces[i][j] = new Space(); 
+            }
+            
+        }//end of inner for loop
+    }//end of outer for loop
+    spaces[2][2].setCurrent = true;
+    this.currentX = 2;
+    this.currentY =2;
+    spaces[y][x].weapon= new Weapon('sword');
+}
+function Map(){
+
+    this.reduceScore = function(){
+        this.score--;
+    };
+
     //don't need getters and setters
 
-    reduceScore(){
-        this.score--;
-    }
 
     this.move(s)= function(s){
         switch(s){
